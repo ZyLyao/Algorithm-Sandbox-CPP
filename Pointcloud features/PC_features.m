71de025c52f828ab89a91827df79589d8d382705
ZyLyao <invimoonj@gmail.com>
2021-12-23 21:19:50 -0800

    switch workspace

--- Pointcloud features/PC_features.m
+++ Pointcloud features/PC_features.m
@@ -1,15 +1,17 @@
 clear
+close all
+
 
 FLAG_DEBUG = 1;
 FLAG_H_SEP = 1;
-FLAG_SHOW_3D = 1;
+FLAG_SHOW_3D = 0;
 %% geo-fence
 pc = pcread('combined_10.pcd');
 
 proj_pln = [0;0;1;30];
 Lidar_pos_P = [0;0;0];
 if FLAG_H_SEP
-    roi = [-inf inf -inf inf -inf 15];
+    roi = [-inf inf -inf inf -inf 20];
 else
     roi = [-inf inf -inf inf -inf inf];
 end
@@ -52,7 +54,7 @@ end
 %% K-R or KNN local features
 method = 0; %% 0:K-R 1:KNN
 num_nn = 10; % in case of KNN, number of neighbors
-radius = 1; %in case of KR, radius in meters 
+radius = 0.5; %in case of KR, radius in meters 
 
 tree = KDTreeSearcher(pc.Location);
 if method == 0
@@ -134,25 +136,25 @@ pnts_proj = PC_3dTo2d_traceback(pc.Location,Lidar_pos_P,proj_pln);
 
 %% Visualize local features
 if FLAG_DEBUG
-    plot_one_feature_ESS(100,FLAG_SHOW_3D, pc, pnts_proj,"sum", [pc_feature.sum] );
-    plot_one_feature_ESS(101,FLAG_SHOW_3D, pc, pnts_proj,"omnivarriance", [pc_feature.omnivarriance] );
-    plot_one_feature_ESS(102,FLAG_SHOW_3D, pc, pnts_proj,"eigenentropy", [pc_feature.eigenentropy] );
-    plot_one_feature_ESS(103,FLAG_SHOW_3D, pc, pnts_proj,"anisotropy", [pc_feature.anisotropy] );
-    
-    plot_one_feature_ESS(104,FLAG_SHOW_3D, pc, pnts_proj,"planarity", [pc_feature.planarity] );
-    plot_one_feature_ESS(105,FLAG_SHOW_3D, pc, pnts_proj,"linearity", [pc_feature.linearity] );
-    plot_one_feature_ESS(106,FLAG_SHOW_3D, pc, pnts_proj,"curvature", [pc_feature.curvature] );
-    plot_one_feature_ESS(107,FLAG_SHOW_3D, pc, pnts_proj,"sphericity", [pc_feature.sphericity] );
-    plot_one_feature_ESS(108,FLAG_SHOW_3D, pc, pnts_proj,"verticality", [pc_feature.verticality] );
-    
-    plot_one_feature_ESS(109,FLAG_SHOW_3D, pc, pnts_proj,"o1a1", [pc_feature.o1a1] );
-    plot_one_feature_ESS(110,FLAG_SHOW_3D, pc, pnts_proj,"o1a2", [pc_feature.o1a2] );
-    plot_one_feature_ESS(111,FLAG_SHOW_3D, pc, pnts_proj,"o2a1", [pc_feature.o2a1] );
-    plot_one_feature_ESS(112,FLAG_SHOW_3D, pc, pnts_proj,"o2a2", [pc_feature.o2a2] );
-
-    plot_one_feature_ESS(113,FLAG_SHOW_3D, pc, pnts_proj,"h_{above}", [pc_feature.h_above] );
-    plot_one_feature_ESS(114,FLAG_SHOW_3D, pc, pnts_proj,"h_{range}", [pc_feature.h_range] );
-    plot_one_feature_ESS(115,FLAG_SHOW_3D, pc, pnts_proj,"h_{below}", [pc_feature.h_below] );
+%     plot_one_feature_ESS(100,FLAG_SHOW_3D, pc, pnts_proj,"sum", [pc_feature.sum] );
+%     plot_one_feature_ESS(101,FLAG_SHOW_3D, pc, pnts_proj,"omnivarriance", [pc_feature.omnivarriance] );
+%     plot_one_feature_ESS(102,FLAG_SHOW_3D, pc, pnts_proj,"eigenentropy", [pc_feature.eigenentropy] );
+%     plot_one_feature_ESS(103,FLAG_SHOW_3D, pc, pnts_proj,"anisotropy", [pc_feature.anisotropy] );
+%     
+%     plot_one_feature_ESS(104,FLAG_SHOW_3D, pc, pnts_proj,"planarity", [pc_feature.planarity] );
+%     plot_one_feature_ESS(105,FLAG_SHOW_3D, pc, pnts_proj,"linearity", [pc_feature.linearity] );
+%     plot_one_feature_ESS(106,FLAG_SHOW_3D, pc, pnts_proj,"curvature", [pc_feature.curvature] );
+%     plot_one_feature_ESS(107,FLAG_SHOW_3D, pc, pnts_proj,"sphericity", [pc_feature.sphericity] );
+%     plot_one_feature_ESS(108,FLAG_SHOW_3D, pc, pnts_proj,"verticality", [pc_feature.verticality] );
+%     
+%     plot_one_feature_ESS(109,FLAG_SHOW_3D, pc, pnts_proj,"o1a1", [pc_feature.o1a1] );
+%     plot_one_feature_ESS(110,FLAG_SHOW_3D, pc, pnts_proj,"o1a2", [pc_feature.o1a2] );
+%     plot_one_feature_ESS(111,FLAG_SHOW_3D, pc, pnts_proj,"o2a1", [pc_feature.o2a1] );
+%     plot_one_feature_ESS(112,FLAG_SHOW_3D, pc, pnts_proj,"o2a2", [pc_feature.o2a2] );
+% 
+%     plot_one_feature_ESS(113,FLAG_SHOW_3D, pc, pnts_proj,"h_{above}", [pc_feature.h_above] );
+%     plot_one_feature_ESS(114,FLAG_SHOW_3D, pc, pnts_proj,"h_{range}", [pc_feature.h_range] );
+%     plot_one_feature_ESS(115,FLAG_SHOW_3D, pc, pnts_proj,"h_{below}", [pc_feature.h_below] );
 %     
 
 end
@@ -161,37 +163,78 @@ end
 D2R = pi/180;
 thres_v_ang_d = 10; % degrees
 thres_v_ang_r = thres_v_ang_d * D2R;
-
 thres_verticality = cos(thres_v_ang_r);
-linearity = [pc_feature.linearity];
-planarity = [pc_feature.planarity];
-verticality = [pc_feature.verticality];
-curvature = [pc_feature.curvature];
+
+linearity_pc = [pc_feature.linearity];
+planarity_pc = [pc_feature.planarity];
+verticality_pc = [pc_feature.verticality];
+curvature_pc = [pc_feature.curvature];
+
 % find points with large verticality
-idx_v = find(verticality>thres_verticality); 
+idx_v_pc = find(verticality_pc>thres_verticality); 
+pc_v = select(pc,idx_v_pc);
+
 % find points with small verticality
+idx_h_pc = find(verticality_pc< 1 - thres_verticality);
+pc_h = select(pc,idx_h_pc);
+
+% Find a proper height for separating deck points and others
+h_med = median(pc_h.Location(:,3));
+deck_margin = 2;% set a 2 meters margin
+h_med_low_bound = h_med - deck_margin; 
+
+% Define a bounding box to remove partial pile points 
+box_rm = [-inf inf -inf inf -inf inf]; % initialize the box to remove points inside of the walls
+box_rm_margin = 5; % meters
+idx_rm_somePile = find( ...
+    pc.Location(:,1) > min(pc_v.Location(:,1)) + box_rm_margin & ...
+    pc.Location(:,1) < max(pc_v.Location(:,1)) - box_rm_margin & ...
+    pc.Location(:,2) > min(pc_v.Location(:,2)) + box_rm_margin & ...
+    pc.Location(:,2) < max(pc_v.Location(:,2)) - box_rm_margin & ...
+    pc.Location(:,3) < h_med_low_bound);
+idx_rm = idx_rm_somePile;
+
+% Get cleaned point cloud and features
+idx_kept = setdiff(1:pc.Count, idx_rm);
+pc_cln = select(pc,idx_kept);
+linearity = linearity_pc(idx_kept);
+planarity = planarity_pc(idx_kept);
+verticality = verticality_pc(idx_kept);
+curvature = curvature_pc(idx_kept);
+
+idx_v = find(verticality>thres_verticality); 
 idx_h = find(verticality< 1 - thres_verticality);
 idx_pln = find(planarity > 0.86);
-idx_upper = find(pc.Location(:,3)>h_med_margin);
-idx_lower = find(pc.Location(:,3)<h_med_margin);
 idx_edge = find(linearity > 0.5);
 idx_curv = find(curvature > 0.1);
 
-pc_h = select(pc,idx_h);
-% Find a proper height for separating deck points and others
-h_med = median(pc_h.Location(:,3));
-h_med_margin = h_med - 2; % set a 2 meters margin
+pc_v_cln = select(pc_cln,idx_v);
+pc_h_high_cln = select(pc_cln, idx_h);
+pc_edge = select(pc_cln, intersect(idx_edge,idx_curv));
 
 
-pc_v = select(pc,idx_v);
-pc_h_high = select(pc, intersect(idx_h,idx_upper));
-pc_edge = select(pc, intersect(idx_edge,idx_curv));
+pc_v_cln_denoise = pcdenoise(pc_v_cln);
+pc_h_high_cln_denoise = pcdenoise(pc_h_high_cln);
+
 
 figure(200)
-scatter3(pc_v.Location(:,1),pc_v.Location(:,2),pc_v.Location(:,3),'.r')
+scatter3(pc_v_cln.Location(:,1),pc_v_cln.Location(:,2),pc_v_cln.Location(:,3),'.r')
 hold on 
-scatter3(pc_h_high.Location(:,1),pc_h_high.Location(:,2),pc_h_high.Location(:,3),'.b')
-scatter3(pc_edge.Location(:,1),pc_edge.Location(:,2),pc_edge.Location(:,3),'.g')
+scatter3(pc_h_high_cln.Location(:,1),pc_h_high_cln.Location(:,2),pc_h_high_cln.Location(:,3),'.b')
+% scatter3(pc_edge.Location(:,1),pc_edge.Location(:,2),pc_edge.Location(:,3),'.g')
+hold off
+axis equal
+xlabel('x, m')
+ylabel('y, m')
+zlabel('z, m')
+title('refine map with verticality')
+legend({'vertical','hort & planar & high','edge'})
+
+figure(201)
+scatter3(pc_v_cln_denoise.Location(:,1),pc_v_cln_denoise.Location(:,2),pc_v_cln_denoise.Location(:,3),'.r')
+hold on 
+scatter3(pc_h_high_cln_denoise.Location(:,1),pc_h_high_cln_denoise.Location(:,2),pc_h_high_cln_denoise.Location(:,3),'.b')
+% scatter3(pc_edge.Location(:,1),pc_edge.Location(:,2),pc_edge.Location(:,3),'.g')
 hold off
 axis equal
 xlabel('x, m')
